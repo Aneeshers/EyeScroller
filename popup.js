4df4b2911c78d1dc22b4b2a178cf98c680e3ae03
Aneeshers <aneeshmuppidi19@gmail.com>
2017-11-12 10:30:18 -0500

    Added Prototype JS

---
+++ popup.js
@@ -0,0 +1,11 @@
+chrome.storage.local.get(null, function(items) { // null implies all items
+    // Convert object to a string.
+    var result = JSON.stringify(items);
+
+    // Save as file
+    var url = 'data:application/json;base64,' + btoa(result);
+    chrome.downloads.download({
+        url: url,
+        filename: 'filename_of_exported_file.txt'
+    });
+});
